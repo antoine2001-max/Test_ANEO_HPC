68cf19315d27741cc39c49b29ebc77afadbec5b2
antoine2001-max <74531904+antoine2001-max@users.noreply.github.com>
2022-03-30 11:38:52 +0200

    Update IDA.py

--- IDA.py
+++ IDA.py
@@ -275,7 +275,7 @@ fn = IDA_star(root = Node(), graph=graph).find_best_path()
 from collections import defaultdict
 from itertools import cycle
 from typing import Dict, Tuple
-#import matplotlib.pyplot as plt
+
 import random
 
 def cycle(lst):
@@ -283,37 +283,7 @@ def cycle(lst):
     lst.append(x)
     return x
 
-#def plot_schedule(node, critical_path=[]) -> None:
-    #schedule = node.schedule
-    #colors_by_proc = defaultdict(lambda:
-        #['tab:blue', 'tab:orange', 'tab:green', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan'])
-    #for id, (start, end, proc) in schedule.items():
-        ## cycle through the colors for this processor
-        #color = cycle(colors_by_proc[proc])
-        #colors_by_proc[proc].append(color)
-        
-        ## handle critical path nodes
-        #if id in critical_path:
-            #critical_kwargs = {
-                #'edgecolor': 'red',
-                #'lw': 2,
-                #'zorder': 100,
-            #}
-        #else:
-            #critical_kwargs = {}
-        
-        # blot the bar and text
-        #plt.broken_barh([(start, end-start)],
-                        #(proc-.4, .8),
-                        #facecolors=color,
-                        #**critical_kwargs)
-        #plt.annotate(str(id),
-                        #xy=((start+end)/2, proc),
-                        #ha='center',
-                        #va='center',
-                        #zorder=101)
-    #plt.yticks(list(colors_by_proc.keys()), [f'Proc {proc}' for proc in colors_by_proc.keys()])
-    #plt.tight_layout()
+
 
 print(fn)
 #plot_schedule(fn)
