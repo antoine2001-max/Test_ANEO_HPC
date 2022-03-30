dc386eddd4f2959beb0730514bf0c2fef500b10d
antoine2001-max <74531904+antoine2001-max@users.noreply.github.com>
2022-03-30 10:16:39 +0200

    Update IDA.py

--- IDA.py
+++ IDA.py
@@ -6,8 +6,8 @@ from copy import deepcopy
 import bisect
 
 # from config import filename, n_cores
-#filename = "./smallRandom.json"
-filename = "./smallComplex.json"
+filename = "./smallRandom.json"
+#filename = "./smallComplex.json"
 n_cores = 2
 print(filename)
 
