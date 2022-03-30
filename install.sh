f469a0bb772dde999038c168755266c793237b42
antoine2001-max <74531904+antoine2001-max@users.noreply.github.com>
2022-03-30 10:14:01 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+# !/bin/sh
+
+sudo apt update -y
+sudo apt install python3-pip -y
+pip install numpy
+sudo apt-get install -y --no-install-recommends openmpi-bin
+sudo apt-get install -y libopenmpi-dev
+sudo pip3 install -r requirements_node.txt
