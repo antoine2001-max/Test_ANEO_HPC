0f6ccdb68eebc63de95f5ecc8464be3a7294ad8a
antoine2001-max <74531904+antoine2001-max@users.noreply.github.com>
2022-03-31 08:49:59 +0200

    Update IDA.py

--- IDA.py
+++ IDA.py
@@ -6,6 +6,7 @@ from copy import deepcopy
 import bisect
 import time
 
+
 # from config import filename, n_cores
 filename = "./smallRandom.json"
 #filename = "./smallComplex.json"
@@ -217,6 +218,7 @@ class Node():
 import queue
 import random as rd
 import sys
+import matplotlib.pyplot as plt
 
 def rdraise(x = 0.1):
     if rd.random() < x:
@@ -302,10 +304,45 @@ def cycle(lst):
     lst.append(x)
     return x
 
+def plot_schedule(node, critical_path=[]) -> None:
+    schedule = node.schedule
+    colors_by_proc = defaultdict(lambda:
+        ['tab:blue', 'tab:orange', 'tab:green', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan'])
+    for id, (start, end, proc) in schedule.items():
+        # cycle through the colors for this processor
+        color = cycle(colors_by_proc[proc])
+        colors_by_proc[proc].append(color)
+        
+        # handle critical path nodes
+        if id in critical_path:
+            critical_kwargs = {
+                'edgecolor': 'red',
+                'lw': 2,
+                'zorder': 100,
+            }
+        else:
+            critical_kwargs = {}
+        
+        # blot the bar and text
+        plt.broken_barh([(start, end-start)],
+                        (proc-.4, .8),
+                        facecolors=color,
+                        **critical_kwargs)
+        plt.annotate(str(id),
+                        xy=((start+end)/2, proc),
+                        ha='center',
+                        va='center',
+                        zorder=101)
+    plt.yticks(list(colors_by_proc.keys()), [f'Proc {proc}' for proc in colors_by_proc.keys()])
+    plt.tight_layout()
+
+print(fn)
+plot_schedule(fn)
+
 
 
 print(fn)
-#plot_schedule(fn)
+plot_schedule(fn)
 
 p = fn.parent
 p.successors()
