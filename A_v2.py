a6f994fdbb23c050e67f1bbf1f82b2f991d657f3
antoine2001-max <74531904+antoine2001-max@users.noreply.github.com>
2022-03-31 11:21:08 +0200

    Update A_v2.py

--- A_v2.py
+++ A_v2.py
@@ -106,7 +106,7 @@ class Graph:
         self.tasks_to_sbl = task2sbl
         self.tasks_to_parent = task2parents
         self.tasks_to_child = task2childs
-        self.n_cores = 2
+        self.n_cores = n_cores
         self.nodes = list()
 
 
