284750029db051bff3a9da487297c34831942a36
antoine2001-max <74531904+antoine2001-max@users.noreply.github.com>
2022-03-31 08:43:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt update -y
 sudo apt install python3-pip -y
 pip3 install numpy
-#sudo apt-get install python3-matplotlib
+sudo apt-get install python3-matplotlib
 sudo apt-get install -y --no-install-recommends openmpi-bin
 sudo apt-get install -y libopenmpi-dev
 sudo pip3 install -r requirements_node.txt
