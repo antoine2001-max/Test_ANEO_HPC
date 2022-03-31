6ed27c85d3c7d44dc8964f1d11c9764d9ead2865
antoine2001-max <74531904+antoine2001-max@users.noreply.github.com>
2022-03-31 09:46:12 +0200

    Update IDA.py

--- IDA.py
+++ IDA.py
@@ -10,7 +10,7 @@ import time
 # from config import filename, n_cores
 #filename = "./smallRandom.json"
 #filename = "./smallComplex.json"
-filename = "./mediumRandom.json"
+filename = "./MediumComplex.json"
 n_cores = 2
 print(filename)
 
