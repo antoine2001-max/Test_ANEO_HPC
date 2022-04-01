b029b58ffed1a229bc747547536bbd4c8efaf4d6
antoine2001-max <74531904+antoine2001-max@users.noreply.github.com>
2022-04-01 10:10:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt update -y
 sudo apt install python3-pip -y
 sudo apt install python3-pandas
 pip install ipyparallel
+sudo apt-get install -y python3-mpi4py
 pip3 install numpy
 sudo apt-get install python3-matplotlib
 sudo apt-get install -y --no-install-recommends openmpi-bin
